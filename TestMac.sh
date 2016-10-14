44e0052402340ae7bf02abdf301db48b445b1124
Yanchen Wu <Yanchen@ycmac.local>
2016-10-14 14:46:02 -0700

    fix mac script

--- TestMac.sh
+++ TestMac.sh
@@ -15,12 +15,12 @@ waitForUrl() {
 
 if [ "$1" == "-v" ]; then
 	echo "Testing w/ volume mapping:"
-	docker-compose -f src/DockerPerf/bin/Release/netcoreapp1.0/publish/docker-compose.volume.mapped.yml up -d & waitForUrl http://localhost:35002 | gnomon -t elapsed-total
+	{ docker-compose -f src/DockerPerf/bin/Release/netcoreapp1.0/publish/docker-compose.volume.mapped.yml up -d; waitForUrl http://localhost:35002; } | gnomon -t elapsed-total
 
 	docker-compose -f src/DockerPerf/bin/Release/netcoreapp1.0/publish/docker-compose.volume.mapped.yml down --remove-orphans
 else
 	echo "Testing w/o volume mapping:"
-	docker-compose -f src/DockerPerf/bin/Release/netcoreapp1.0/publish/docker-compose.yml up -d & waitForUrl http://localhost:35001 | gnomon -t elapsed-total
+	{ docker-compose -f src/DockerPerf/bin/Release/netcoreapp1.0/publish/docker-compose.yml up -d; waitForUrl http://localhost:35001; } | gnomon -t elapsed-total
 
 	docker-compose -f src/DockerPerf/bin/Release/netcoreapp1.0/publish/docker-compose.yml down --remove-orphans
 fi
